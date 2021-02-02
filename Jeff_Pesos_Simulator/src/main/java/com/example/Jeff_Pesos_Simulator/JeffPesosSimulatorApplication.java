fe8583ad3564bea270aa41eff72440ff1f7a971b
UlysseCabanes <57804093+UlysseCabanes@users.noreply.github.com>
2021-02-02 11:26:00 +0100

    Initial commit

---
+++ Jeff_Pesos_Simulator/src/main/java/com/example/Jeff_Pesos_Simulator/JeffPesosSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.Jeff_Pesos_Simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeffPesosSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeffPesosSimulatorApplication.class, args);
+	}
+
+}
---
+++ Jeff_Pesos_Simulator/src/test/java/com/example/Jeff_Pesos_Simulator/JeffPesosSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Jeff_Pesos_Simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeffPesosSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
