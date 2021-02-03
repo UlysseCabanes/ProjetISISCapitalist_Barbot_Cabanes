9a66d40612ef35e53589d3f39026b89b1adae03d
MatteoBarbot2 <57804265+MatteoBarbot2@users.noreply.github.com>
2021-02-03 10:17:48 +0100

    Client update

--- JeffPesosClient/src/app/app.component.ts
+++ JeffPesosClient/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { World, Pallier, Product } from './world';
+import { RestserviceService } from './restservice.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
+  world: World = new World();
+  server: string;
   title = 'JeffPesosClient';
+  constructor(private service: RestserviceService) {
+    this.server = service.getServer;
+    service.getWorld().then(
+    world => {
+    this.world = world;
+    });
+  }
 }
