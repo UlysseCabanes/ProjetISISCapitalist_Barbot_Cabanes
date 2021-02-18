4c094d8ddf141c6ed012594769de93a5f0fdd9d7
UlysseCabanes <57804093+UlysseCabanes@users.noreply.github.com>
2021-02-18 12:10:42 +0100

    Badge managers

--- JeffPesosClient/src/app/app.component.ts
+++ JeffPesosClient/src/app/app.component.ts
@@ -19,28 +19,57 @@ export class AppComponent {
   constructor(private service: RestserviceService, private snackBar: MatSnackBar) {
     this.server = service.getServer;
     service.getWorld().then(
-      world => {
-        this.world = world;
-      });
+    world => {
+    this.world = world;
+    this.updateManagersBadges();
+    });
   }
 
   showManagers: boolean = false;
 
+  //Engager un manager
   hireManager(manager: Pallier) {
+    //Si l'argent est suffisant
     if (this.world.money >= manager.seuil) {
+      //Décrémentation de l'argent
       this.world.money -= manager.seuil;
+      //Débloquer le manager
       manager.unlocked = true;
+      //Dans le produit aussi
       this.world.products.product[manager.idcible-1].managerUnlocked = true;
+      //Mise à jour de la valeur du badge des managers
+      this.updateManagersBadges();
+      //Afficher un message de confirmation d'engagement du manager
       this.hireMessage("Vous avez engagé " + manager.name + " comme manager !");
     }
-  };
+  }
 
+  //Affichage du message de confirmation d'engagement du manager
   hireMessage(message : string) : void { 
     this.snackBar.open(message, "", { duration : 5000 })
   }
-
-  onProductionDone(p: Product) {
+  //Production d'un produit
+  onProductionDone(p : Product){
+    //Incrémentation de l'argent
     this.world.money += p.revenu;
+    //Mise à jour de la valeur du badge des managers
+    this.updateManagersBadges();
+  }
+  
+  //Valeur du badge des managers
+  badgeManagers = 0;
+  //Mettre à jour le badge des managers
+  updateManagersBadges() {
+    //réinitialiser la valeur du badge
+    this.badgeManagers = 0;
+    //Parcourir les managers
+    for (let m of this.world.managers.pallier) {
+      //Vérifier si l'argent possédé dépasse le prix du manager
+      if (this.world.money >= m.seuil && !m.unlocked) {
+        //Incrémenter la valeur du badge
+        this.badgeManagers += 1;
+      }
+    }
   }
   cycle() {
     switch (this.qtmulti) {
