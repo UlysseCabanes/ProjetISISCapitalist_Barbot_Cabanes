ad9001293d4194078e5b1e7af61a0387dcbb799b
UlysseCabanes <57804093+UlysseCabanes@users.noreply.github.com>
2021-03-08 11:35:55 +0100

    Angels

--- JeffPesosClient/src/app/app.component.ts
+++ JeffPesosClient/src/app/app.component.ts
@@ -17,7 +17,10 @@ export class AppComponent {
   money = 0;
   username = "";
   service: any;
+  badgeManagers = 0;
   showManagers: boolean = false;
+  badgeAngels = 0;
+  showAngels: boolean = false;
 
   constructor(service: RestserviceService, private snackBar: MatSnackBar) {
     this.server = service.getServer;
@@ -27,9 +30,11 @@ export class AppComponent {
     //Mettre à jour la valeur du badge des managers 
     //dès le chargement de la page
     this.updateManagersBadges();
+    this.updateAngelsBadges();
     this.username = localStorage.getItem("username") || 'Captain' + Math.floor(Math.random() * 10000);
     });
   }
+
   //Engager un manager
   hireManager(manager: Pallier) {
     //Si l'argent est suffisant
@@ -49,27 +54,48 @@ export class AppComponent {
     } 
   }
 
+  //Acheter un ange
+  buyAngel(angel: Pallier) {
+    //Si l'argent est suffisant
+    if (this.world.money >= angel.seuil) {
+      //Décrémentation de l'argent
+      this.world.money -= angel.seuil;
+      //Débloquer l'ange
+      angel.unlocked = true;
+      //Mise à jour de la valeur du badge des anges
+      this.updateAngelsBadges();
+      //Afficher un message de confirmation d'achat de l'ange
+      this.buyAngelMessage("Vous avez acheté l'ange " + angel.name + " !");
+    }
+  }
+
   //Affichage du message de confirmation d'engagement du manager
   hireMessage(message : string) : void { 
     this.snackBar.open(message, "", { duration : 5000 })
   }
+
+  //Affichage du message de confirmation d'achat de l'ange
+  buyAngelMessage(message : string) : void { 
+    this.snackBar.open(message, "", { duration : 5000 })
+  }
+
   //Production d'un produit
   onProductionDone(p : Product){
     //Incrémentation de l'argent
     this.world.money += p.revenu;
 
     this.service.putProduct(p);
-    //Mise à jour de la valeur du badge des managers
+    //Mise à jour de la valeur des badges
     this.updateManagersBadges();
+    this.updateAngelsBadges();
   }
   onAchatDone(w : World){
     this.world.money = w.money;
-    //Mise à jour de la valeur du badge des managers
+    //Mise à jour de la valeur des badges
     this.updateManagersBadges();
+    this.updateAngelsBadges();
   }
   
-  //Valeur du badge des managers
-  badgeManagers = 0;
   //Mettre à jour le badge des managers
   updateManagersBadges() {
     //réinitialiser la valeur du badge
@@ -83,6 +109,21 @@ export class AppComponent {
       }
     }
   }
+
+  //Mettre à jour le badge des anges
+  updateAngelsBadges() {
+    //réinitialiser la valeur du badge
+    this.badgeAngels = 0;
+    //Parcourir les anges
+    for (let a of this.world.angelupgrades.pallier) {
+      //Vérifier si l'argent possédé dépasse le prix de l'ange
+      if (this.world.money >= a.seuil && !a.unlocked) {
+        //Incrémenter la valeur du badge
+        this.badgeAngels += 1;
+      }
+    }
+  }
+
   cycle() {
     switch (this.qtmulti) {
       case 'x1':
